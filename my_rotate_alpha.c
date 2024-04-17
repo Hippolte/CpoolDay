a6ad8f596731efb0d8fb3835e83544557aacd7db
Hippolyte <167294188+Hippolte@users.noreply.github.com>
2024-04-17 13:26:41 +0000

    changements

---
+++ my_rotate_alpha.c
@@ -0,0 +1,19 @@
+void print_char(char c);
+
+char *my_rotate_alpha(char *sentance) {
+    char *alphabet = "abcdefghijklmnopqrstuvwxyz";
+    for (int i = 0; sentance[i] = '\0'; i++) {
+        int lettre = 0;
+        for (int j = 0; alphabet[j] != '\0'; j++) {
+            if (sentance[i] == alphabet[j]) {
+                lettre = j;
+            }
+        lettre *= 2;
+        while (lettre > 26) {
+            lettre -= 26;
+        }
+        sentance[i] = alphabet[lettre];
+        }
+    }
+    return sentance;
+}
