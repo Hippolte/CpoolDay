b8bf60a4cbd06220fca97a4102afba458418f423
Hippolyte <167294188+Hippolte@users.noreply.github.com>
2024-04-17 14:00:11 +0000

    aaaaaaaaaa

--- my_sort.c
+++ my_sort.c
@@ -1,18 +1,17 @@
 void print_number(int value);
 
 void my_sort(int *nlist, int size) {
-    char *list = nlist
     for (int i = 0; i < size; i++) {
         int nombre_tres_haut = 100000;
-        int min = list[0];
+        int min = nlist[0];
         int indice_min = 0;
         for (int j = 0; j < size; j++) {
-            if (list[j] < min) {
-                min = list[j];
+            if (nlist[j] < min) {
+                min = nlist[j];
                 indice_min = j;
             }
         }
         print_number(min);
-        list[indice_min] = nombre_tres_haut;
+        nlist[indice_min] = nombre_tres_haut;
     }
 }
