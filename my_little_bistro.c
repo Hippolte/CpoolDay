9b388f2ac87cd667fa7fcb7959ea01495e46f81f
Hippolyte <167294188+Hippolte@users.noreply.github.com>
2024-04-17 11:45:16 +0000

    changements

---
+++ my_little_bistro.c
@@ -0,0 +1,20 @@
+int my_little_bistro(int value1, char op, int value2) {
+    if (op == '+') {
+        return value1 + value2;
+    }
+    if (op == '-') {
+        return value1 - value2;
+    }
+    if (op == '*') {
+        return value1 * value2;
+    }
+    if (op == '/') {
+        return value1 / value2;
+    }
+    if (op == '%') {
+        return value1 % value2;
+    }
+    else {
+        return 0;
+    }
+}
